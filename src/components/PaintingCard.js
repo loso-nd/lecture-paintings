542e29e6afea0dca6575cb0e9ffbc57fed161d54
Nahum <losond8@gmail.com>
2021-03-23 12:35:17 -0400

    Learning how to deconstruct my props.

--- src/components/PaintingCard.js
+++ src/components/PaintingCard.js
@@ -1,11 +1,18 @@
 const PaintingCard = props => {
+    let {image, title, artist, date, dimensions} = props.painting
     return (
         <div>
-            <img src ={props.painting.image} />
-            <h4>"{props.painting.title}" by {props.painting.artist.name}</h4>
-            <p>Year: {props.painting.date}</p>
-            <p>Dimensions: {props.painting.dimensions.width} in. x {props.painting.dimensions.height}</p>
-        </div>
+            <img src ={image} alt={artist.name}/>
+            <h4>"{title}" by {artist.name}</h4>
+            <p>Year: {date}</p>
+            <p>Dimensions: {dimensions.width} in. x {dimensions.height}</p>
+        </div>       
+        // <div>
+        //     <img src ={props.painting.image} />
+        //     <h4>"{props.painting.title}" by {props.painting.artist.name}</h4>
+        //     <p>Year: {props.painting.date}</p>
+        //     <p>Dimensions: {props.painting.dimensions.width} in. x {props.painting.dimensions.height}</p>
+        // </div>
     )
 }
 
